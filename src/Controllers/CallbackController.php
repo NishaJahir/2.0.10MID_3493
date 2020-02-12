e8e5f3b3a4f507285dfb08d3481e23ad7eaad663
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-02-12 11:40:40 +0530

    Update CallbackController.php

--- src/Controllers/CallbackController.php
+++ src/Controllers/CallbackController.php
@@ -901,7 +901,7 @@ class CallbackController extends Controller
 	    
 	           $system_version   = NovalnetConstants::PLUGIN_VERSION;
                    $notify_url       = $this->webstoreHelper->getCurrentWebstoreConfiguration()->domainSsl . '/payment/novalnet/callback/';
-	           $mailContent .= 'Date: '. $this->aryCaptureParams['ta_date'] . '<br/> Time: '. $this->aryCaptureParams['ta_time'] . '<br/> URL: '. $notify_url . '<br/> Email: '. $this->aryCaptureParams['email'] . '<br/> Amount: ' . $this->aryCaptureParams['amount'] . $this->aryCaptureParams['amount'] . ' ' . $this->aryCaptureParams['currency'] . '<br/> Systemname: Plentymarkets '<br/>  Version: '. $shop_version . '<br/> Please refer the attached file (password protected) for the order details. <br/> Contact Novalnet technic team for support. ';
+	           $mailContent .= 'Date: '. $this->aryCaptureParams['ta_date'] . '<br/> Time: '. $this->aryCaptureParams['ta_time'] . '<br/> URL: '. $notify_url . '<br/> Email: '. $this->aryCaptureParams['email'] . '<br/> Amount: ' . $this->aryCaptureParams['amount'] . $this->aryCaptureParams['amount'] . ' ' . $this->aryCaptureParams['currency'] . '<br/> Systemname: Plentymarkets <br/>  Version: '. $shop_version . '<br/> Please refer the attached file (password protected) for the order details. <br/> Contact Novalnet technic team for support. ';
 		   $mailer = pluginApp(MailerContract::class);
                    $mailer->sendHtml($mailContent, $toAddress, $subject);
 	           
